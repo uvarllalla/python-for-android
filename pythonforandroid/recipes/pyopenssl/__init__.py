f0177c5e72f7a604d0ac0f6d36b2c9c97a0f5b42
Robert Niederreiter <rnix@squarewave.at>
2024-11-27 11:46:44 +0100

    Try older version

--- pythonforandroid/recipes/pyopenssl/__init__.py
+++ pythonforandroid/recipes/pyopenssl/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class PyOpenSSLRecipe(PythonRecipe):
-    version = '24.2.1'
+    version = '24.1.0'
     url = 'https://pypi.python.org/packages/source/p/pyOpenSSL/pyOpenSSL-{version}.tar.gz'
     depends = ['cffi', 'openssl', 'setuptools']
     site_packages_name = 'OpenSSL'
